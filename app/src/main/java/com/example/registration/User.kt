78728e00e4f9b46691866e3ed51406114e113d20
DaniyarZhenis <aldik.zhen@gmail.com>
2024-03-01 21:04:27 +0600

    change getusers

--- app/src/main/java/com/example/registration/User.kt
+++ app/src/main/java/com/example/registration/User.kt
@@ -3,6 +3,6 @@ package com.example.registration
 class User(val login: String, val email: String, val password: String) {
 
     fun getUsers() : String {
-        return "$login, $email, $password"
+        return "Login:$login, email: $email, password: $password"
     }
 }
